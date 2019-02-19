5de9551d0e4e45154f6be45c68863d64f8778f55
John Fercher <johnathanfercher22@gmail.com>
2019-02-19 20:01:35 -0300

    teste de error_handler

---
+++ tests/application/filters/error_handler_test.py
@@ -0,0 +1,26 @@
+from flask import Response
+
+from src.application.filters.error_handler import error_handler
+
+
+def test_when_raise_error_should_return_500():
+
+    @error_handler
+    def raise_error():
+        raise ValueError('A very specific bad thing happened.')
+
+    error_handled_result = raise_error()
+    response = Response(status=500)
+
+    assert error_handled_result.status == response.status
+
+
+def test_when_not_raise_error_should_return_200():
+
+    @error_handler
+    def raise_error():
+        return "ok"
+
+    result = raise_error()
+
+    assert result == "ok"
